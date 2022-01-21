7b33812b01bb62f9db5e83a69662ed144b8d8d09
LinJie09 <zxc664231@gmail.com>
2022-01-21 19:05:51 +0800

    changed

--- PracticeJs2/dice.js
+++ PracticeJs2/dice.js
@@ -3,6 +3,10 @@ console.log('HELLO')
 function func(){
     const dice = document.getElementById('dices')
     console.log(dice.value)
+    const count = document.getElementById('count')
+    console.log(count.value)
+    const times = document.getElementById('times')
+    console.log(times.value)
 }
 
 
