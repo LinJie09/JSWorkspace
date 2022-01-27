aad15beb7d189e1768a7a013cfd932c3d56aff1c
LinJie09 <zxc664231@gmail.com>
2022-01-27 13:04:41 +0800

    changed

--- PracticeJs2/dice.js
+++ PracticeJs2/dice.js
@@ -1,5 +1,7 @@
 console.log('HELLO')
 
+
+
 function func(){
     const dice = document.getElementById('dices')
     console.log(dice.value)
--- PracticeJs2/index.js
+++ PracticeJs2/index.js
@@ -1,26 +1,24 @@
 // 九九乘法表
+    
+window.onload = () => {
+        const table = document.getElementById('form') 
+        for (let i = 1; i <= 9; i++) {
+            const tr = document.createElement('tr')
+            table.appendChild(tr)
+        for (let j = 1; j <= 9; j++) {
+            const td = document.createElement('td')
+            mult = (i + '*' + j + '=' + i * j).toString()
+            td.innerHTML = mult
+            tr.appendChild(td)
+        }
+    }   
+    }       
 
-let i ,j,mult
-let f1 = document.getElementById('form')
-let tr = document.createElement('t1')
-let td = document.createElement('d1')
-for ( i =1;i<=9;i++)
-{
-    for( j=1;j<=9;j++)
-    {
-        mult = (i+'*'+j+'='+i*j)
-    }
-}
-const hThree = document.createElement('h3')
-const text = document.createTextNode('Ha man')
-hThree.appendChild(text)
-const app = document.getElementById('app')
-app.appendChild(hThree)
 
 
 
 /*function Mult ()
-{   
+{
         for (let i =1;i<=9;i++)
     {
             for(let j=1;j<=9;j++)
