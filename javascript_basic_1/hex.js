56ad6aac1c0e44a965bf79c096557ae03aa515a6
LinJie09 <zxc664231@gmail.com>
2022-02-01 21:21:52 +0800

    changed

--- javascript_basic_1/hex.js
+++ javascript_basic_1/hex.js
@@ -1,1 +1,27 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+window.onload = () => {
+    const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+
+    const btn = document.getElementById('btn')
+
+    btn.addEventListener('click', function () {
+        let randomWord = '#'
+        for(let i=0;i<6;i++){
+            randomWord += hex[click()]
+        }
+
+        document.body.style.backgroundColor = randomWord
+        document.querySelector(".color").textContent = randomWord
+
+    })
+
+    function click() {
+        return Math.floor(Math.random() * hex.length)
+    }
+}
+
+
+
+
