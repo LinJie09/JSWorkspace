264a0442b47cfeca51568beff49eec1886cd12fc
LinJie09 <zxc664231@gmail.com>
2022-02-04 21:50:41 +0800

    changed

---
+++ 02-counter/setup/app.js
@@ -0,0 +1,14 @@
+
+window.onload = () => {
+
+    let count = 0
+    const btn = document.querySelectorAll('btn')
+    const value = document.querySelector('#value')
+    btn.forEach(function () {
+        btn.addEventListener('click',function(e){
+            const number = e
+            
+        })
+        
+    });
+}
